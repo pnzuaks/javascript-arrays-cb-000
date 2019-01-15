0e652b6e05257d4ebe6c78697011d4d3303e5c29
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-15 22:50:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,10 +46,11 @@ function removeElementFromEndOfArray(array) {
 
 
 
-
+----------------------------------
 
 log();
 function log() {
   console.log(greeting);
   var greeting = 'Hello!';
 }
+  var x = 'this is x'
